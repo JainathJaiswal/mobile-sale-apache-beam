8cc19120284889188a15321af1c09401cbf1c887
JainathJaiswal <30718665+JainathJaiswal@users.noreply.github.com>
2019-04-07 12:08:40 +0530

    Update MobileSaleTest.java

--- src/test/java/com/jai/MobileSaleTest.java
+++ src/test/java/com/jai/MobileSaleTest.java
@@ -10,7 +10,7 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class MobileSaleTest {
 	@Test
-	public void testDebuggingWordCount() throws Exception {
+	public void testDebuggingMobileSale() throws Exception {
 
 		MobileSaleOptions options = TestPipeline.testingPipelineOptions().as(MobileSaleOptions.class);
 		options.setInputFile("mobile_sale_list.csv");
